b9340767c50d76183a6a2279b1035b3daddb5305
tamtran22 <hocsinhchuyentin@gmail.com>
2022-12-21 11:00:03 +0900

    minor change

--- batching_graph/data.py
+++ batching_graph/data.py
@@ -32,15 +32,3 @@ class GraphData:
             if _u == v:
                 return 1
         return 0
-        
-
-if __name__ == '__main__':
-    n_node = 56891
-    node_dim = 4
-    n_edge = 302315
-    edge_dim = 4
-    x = np.random.random(size=(n_node, node_dim))
-    edge_index = np.random.randint(low=0, high=n_node, size=(2,n_edge))
-    edge_attr = np.random.random(size=(n_edge, edge_dim))
-    
-    data = GraphData(x, edge_index, edge_attr)
