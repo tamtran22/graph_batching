5284e5c57d1cfe2d5afaed1deb8cca3974f930f5
tamtran22 <hocsinhchuyentin@gmail.com>
2022-12-28 15:31:36 +0900

    dd

--- preprocessing/batching.py
+++ preprocessing/batching.py
@@ -1,12 +1,11 @@
 import numpy as np
 from data import GraphData
-from scipy import sparse
-# from sklearn.cluster import SpectralClustering
-import metis
+
 import networkx as nx
 
+import metis
 
-from sknetwork.clustering import Louvain
+from sknetwork.clustering import Louvain, PropagationClustering
 from sklearn.metrics.cluster import normalized_mutual_info_score
 
 def metis_batching(data : GraphData, relative_batch_size : int = 100, recursive : bool = False):
@@ -46,7 +45,26 @@ def louvain_batching(data : GraphData, relative_batch_size : int = 100, recursiv
     graph = nx.from_edgelist(edgelist=edgelist)
     
     adj = nx.adjacency_matrix(graph)
-    louvain = Louvain()
-    clusters = louvain.fit_transform(adj)
+    louvain = Louvain(resolution=1)
+    parts = louvain.fit_transform(adj)
+    # propagation = PropagationClustering()
+    # parts = propagation.fit_transform(adj)
 
-    print(clusters)
+    batchs = []
+    for i in range(n_batch):
+        batch_node = np.where(np.array(parts) == i)[0]
+        batch_edge = np.isin(data.edge_index, batch_node)
+        if recursive:
+            batch_edge = np.logical_or(batch_edge[0], batch_edge[1])
+        else:
+            batch_edge = np.logical_and(batch_edge[0], batch_edge[1])
+        batch_edge = np.where(batch_edge == True)[0]
+        batch_edge_index = data.edge_index[:, batch_edge]
+        batch_node = np.where(np.isin(nodelist, batch_edge_index) == True)[0]
+        for j in range(batch_edge_index.shape[1]):
+            batch_edge_index[0,j] = list(batch_node).index(batch_edge_index[0,j])
+            batch_edge_index[1,j] = list(batch_node).index(batch_edge_index[1,j])
+        batch_x = data.x[batch_node]
+        batch_edge_attr = data.edge_attr[batch_edge]
+        batchs.append(GraphData(batch_x, batch_edge_index, batch_edge_attr))
+    return batchs
