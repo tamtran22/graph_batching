2abb8b51f13c52578af1cb2f5dab6dedeafe1f40
tranminhtam <hocsinhchuyentin@gmail.com>
2023-02-23 10:16:59 +0900

    minor change

--- preprocessing/batching_v2.py
+++ preprocessing/batching_v2.py
@@ -1,6 +1,6 @@
 import numpy as np
 from data.data import TorchGraphData
-
+import torch
 import nxmetis
 
 #####################################################################
@@ -8,7 +8,8 @@ def get_batch_x(data : TorchGraphData, partition : np.array):
     return data.x[partition]
 
 def get_batch_edge_index(data : TorchGraphData, partition : np.array):
-    
+    _batch_edge_index = torch.isin(data.edge_index, torch.tensor(partition))
+    print(torch.argwhere(_batch_edge_index))
 
 def get_batch_edge_attr(data : TorchGraphData, partition : np.array):
     pass
@@ -19,5 +20,8 @@ def get_batch_graphs(data : TorchGraphData, batch_size : int, recursive : bool):
         G=data.graph,
         nparts=n_batchs
     )
+
+    partition = partitions[0]
+    get_batch_edge_index(data, partition)
 
     
